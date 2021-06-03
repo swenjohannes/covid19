0c0b6649a85e1f28a8cb8b7eb5b9174007506d25
PeterBerhuis <48983347+PeterBerhuis@users.noreply.github.com>
2021-06-03 15:22:08 +0200

    Update load_hospital.py

--- load_hospital.py
+++ load_hospital.py
@@ -1,20 +1,30 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri May  7 11:28:26 2021
-
-@author: Swen, Peter, Rob
+Created on Fri May  7 11:28:31 2021
+@author: Peter,Swen,Rob
 """
 
-#Import Required Libraries 
+#Libraries
 import pandas as pd
 
-#Import hospital data
-url = 'https://raw.githubusercontent.com/mzelst/covid-19/master/data/all_data.csv'
-dfHos = pd.read_csv(url, sep=',')
-dfHos = dfHos[['date', 'positivetests', 'Hospital_Intake_Proven']]
-dfHos.rename(columns = {'date':'Date',
-                        'positivetests':'Total_reported',
-                        'Hospital_Intake_Proven': 'Hospital_admission'},
-             inplace = True)
+# Import all datasets
+import load_hospital
+import load_sewage
+import load_los
 
-dfHos.Date = pd.to_datetime(dfHos.Date, format = '%Y-%m-%d').dt.date
+#get data
+dfHos = load_hospital.dfHos
+dfS = load_sewage.dfRegion
+dfLoS =  load_los.dfLoS
+
+# Merge hospital & sewage data 
+df = pd.merge(dfS, dfHos, on = ['Date'], how = 'left')
+
+df
+
+# end date of the data used is 2021-05-23
+df.index = df['Date']
+df=df.drop(columns = 'Date')
+df.index = pd.to_datetime(df.index)
+before_end_date = df.index<='2021-05-23'
+df=df[before_end_date]
