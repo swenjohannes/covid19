e29ade6eba648c246e2609d770cd01922a51057f
swenjohannes <74203742+swenjohannes@users.noreply.github.com>
2021-06-03 14:39:29 +0200

    Update load_hospital.py

--- load_hospital.py
+++ load_hospital.py
@@ -9,28 +9,11 @@ Created on Fri May  7 11:28:26 2021
 import pandas as pd
 
 #Import hospital data
-url = 'https://data.rivm.nl/covid-19/COVID-19_aantallen_gemeente_per_dag.csv'
-dfHos = pd.read_csv(url, sep=';')
-dfHos.rename(columns = {'Date_of_publication' : 'Date'}, inplace = True)
-dfHos['Date'] = pd.to_datetime(dfHos['Date'], format = '%Y-%m-%d').dt.date
-
-
-#group by date and province & aggregate
-dfHos = dfHos.groupby(['Date', 'Province']).agg({'Hospital_admission': 'sum', 
-                                          'Total_reported' : 'sum'}).reset_index()
-
-regions = {'Drenthe' : 'Dr',
-          'Flevoland' : 'Fl', 
-          'Fryslân' : 'Fr', 
-          'Gelderland' : 'Ge',
-          'Groningen' : 'Gr',
-          'Limburg' : 'Lm', 
-          'Noord-Brabant' : 'Nb', 
-          'Noord-Holland' : 'Nh',
-          'Overijssel' : 'Ov',
-          'Utrecht' : 'Ut', 
-          'Zeeland' : 'Ze', 
-          'Zuid-Holland' : 'Zh'}
-
-dfHos.replace(regions, inplace = True)
+url = 'https://raw.githubusercontent.com/mzelst/covid-19/master/data/all_data.csv'
+dfHos = pd.read_csv(url, sep=',')
+dfHos = dfHos[['date', 'positivetests', 'Hospital_Intake_Proven']]
+dfHos.rename(columns = {'date':'Date',
+                        'positivetests':'Total_reported',
+                        'Hospital_Intake_Proven': 'Hospital_admission'},
+             inplace = True)
 
